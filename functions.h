87921513d7f0bd5d0ec4005b696ffb875b50db5a
yuina-blend <moon.h14.dd2.as@gmial.com>
2022-10-28 15:34:24 +0900

    Add comment

--- functions.h
+++ functions.h
@@ -4,6 +4,6 @@
 int TitForTat(int ID, int n, int SC[2], int *H); //しっぺ返し戦法 弘中
 int roma(int ID, int n, int SC[2], int *H);
 int tatsuki(int ID, int n, int SC[2], int *H); //直近10試合の相手の手の割合をみて,多い方の真似をする. 野口
-int Friedman(int ID, int n, int SC[2], int *H);
+int Friedman(int ID, int n, int SC[2], int *H); //最初から常に協力するが、一度でも裏切られれば以降すべて裏切る(フリードマン戦略) 弘中
 
 #endif
