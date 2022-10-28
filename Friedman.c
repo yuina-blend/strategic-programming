8109e2cbdd9cd3f7bf3b9bde9054759a0944effd
yuina-blend <moon.h14.dd2.as@gmial.com>
2022-10-28 15:14:44 +0900

    Create フリードマン戦略 テスト用

---
+++ Friedman.c
@@ -0,0 +1,18 @@
+int Friedman(int ID, int n, int SC[2], int *H)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+    else
+    {
+        for (int i; i < n; i++)
+        {
+            if (*(H + 2 * i) == 1)
+            {
+                return 1;
+            }
+        } //一度でも裏切ったら以降すべて裏切る
+        return 0;
+    }
+}
