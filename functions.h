555b0a14771de81e69b78ef83cdb73f609d35bf9
romasw <romat.dev@gmail.com>
2022-10-28 14:04:05 +0900

    roma

--- functions.h
+++ functions.h
@@ -2,5 +2,6 @@
 #define FUNCTIONS
 
 int TitForTat(int ID, int n, int SC[2], int *H); //しっぺ返し戦法 弘中
+int roma(int ID, int n, int SC[2], int *H);
 
 #endif
---
+++ roma.c
@@ -0,0 +1,42 @@
+int roma(int ID,int n,int SC[2] , int *H)
+{
+  int  t=0,i,j,sum=0, sum2=0, ID_op;
+  double pob, pob_r;
+
+  if(n==0) return rand() & 1;
+  if(ID){
+    ID_op = 0;
+  }else{
+    ID_op = 1;
+  }
+  for(i=0;i<n;i++){
+    sum += *(H+2*i+ID_op);
+  }
+  pob = sum/n;
+  if(pob > 0.5){
+    t = 1;
+  }
+  if(n > 100){
+    sum = 0;
+    for(i=n;i>n-100;i--){
+      sum += *(H+2*i+ID_op);;
+    }
+    pob_r = sum/10.0;
+    if(fabs(pob_r - pob) > 0.30){
+      if(pob_r > 0.5){
+        t = 1;
+      }
+    }
+    sum = 0;
+    for(i=n;i>n-10;i--){
+      sum += *(H+2*i+ID_op);
+    }
+    for(i=n;i>n-5;i--){
+      sum2 += *(H+2*i+ID);
+    }
+    if(sum == 0 && sum2 == 0){
+      t = 1;
+    }
+  }
+  return t;
+}
