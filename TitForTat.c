5b10fb2866ed1aef560d63bc63647dbd0daba9e1
yuina-blend <moon.h14.dd2.as@gmial.com>
2022-10-28 13:55:55 +0900

    Fix 条件を修正

--- TitForTat.c
+++ TitForTat.c
@@ -1,11 +1,11 @@
 int TitForTat(int ID, int n, int SC[2], int *H) /*ID:自身のID, 現在のターンはn（0始まり）, SC[ID]:IDの現在の得点*/
 {
     int t;
-    if (n)
+    if (n == 0)
     {
         return 0;
     }
-    else if (ID)
+    else if (ID == 0)
     {
         return *(H + 2 * (n - 1) + 1);
     }
